25a329c8e7984332171848c8db817dd33b9da8c4
mertkoparmal08 <170204022@ogr.alanya.edu.tr>
2022-04-07 02:08:08 +0300

    Artık Yıl Bulma
    Patika Java 101 Artık Yıl Bulma

---
+++ Artık Yıl/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int yil,a;
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Yýl: ");
+		yil=scan.nextInt();
+		
+		if((yil%100)==0) {
+			if((yil%400)==0) {
+				System.out.println(yil+" bir artýk yýldýr !");
+			}else {
+				System.out.println(yil+" bir artýk yýl deðildir !");
+			}
+		}else {
+			if((yil%4)==0) {
+				System.out.println(yil+" bir artýk yýldýr !");
+			}else {
+				System.out.println(yil+" bir artýk yýl deðildir !");
+			}
+		}
+	}
+}
