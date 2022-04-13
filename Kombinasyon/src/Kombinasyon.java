e686a77361794ff0156696c4b6d138d5b9f65f2f
mertkoparmal08 <170204022@ogr.alanya.edu.tr>
2022-04-13 15:30:28 +0300

    Kombinasyon
    Patika Java101 Kombinasyon

---
+++ Kombinasyon/src/Kombinasyon.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Kombinasyon {
+	
+    public static void main(String[] args) {
+    	
+        int n,r,n_faktoriyel=1,r_faktoriyel=1,cýkarma_faktoriyel = 1,sonuc;
+        
+        Scanner input = new Scanner(System.in);
+       
+        System.out.print("N: ");
+        n = input.nextInt();
+        System.out.print("R: ");
+        r = input.nextInt();
+        
+        int cýkarma = n-r;
+        
+        for (int i = 1;i<=n;i++){
+            n_faktoriyel *= i;
+        }
+        
+        for (int j = 1;j<=r;j++){5
+            r_faktoriyel *= j;
+        }
+        
+        for (int k = 1;k<=cýkarma;k++){
+            cýkarma_faktoriyel *= k;
+        }
+        
+        sonuc = n_faktoriyel/(r_faktoriyel*cýkarma_faktoriyel);
+        System.out.print(sonuc);
+    }
+}
